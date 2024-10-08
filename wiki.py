d973758ef93399c0ce508d6e381df5a3a1e1e0ad
epicsharkbait8 <epicsharkbait82@gmail.com>
2024-10-08 18:05:45 +0000

    more stuff

--- wiki.py
+++ wiki.py
@@ -1,3 +1,4 @@
+from queue import Queue
 import wikipediaapi
 import time
 
@@ -14,16 +15,48 @@ def fetch_links(page):
     return links_list
 
 def wikipedia_game_solver(start_page, target_page):
-    links = fetch_links(start_page)
+    print("working on it")
+    start_time = time.time()
 
-    for title in links:
-        if title == "Rose Parade":
-            print("yippie")
-        else:
-            print("it aint here:(")
+    queue = Queue()
+    visited = set()
+    parent = {}
+
+
+    queue.put(start_page.title)
+    visited.add(start_page.title)
+
+
+    while not queue.empty():
+        currently_page_title = queue.get()
+
+        if current_page_title == target_page.titles:
+            break
+
+        current_page = wiki.page(current_page_title)
+        links = fetch_links(current_page)
+
+        if link not in visited:
+            queue.put(link)
+            visited.add(link)
+            parent[link] = currently_page_title
+
+
+    path = []
+    page_title = target_page.title
+    while page_title != start_page.title:
+        path.append(page_title)
+        page_title = parent[page_title]
+
+
+    path.append(start_page.title)   
+    path.reverse()
+
+    end_time = time.time()
+    print("this algorithm took", end_time - start_time, "seconds")
+    return path
 
 start_page = wiki.page("Pasadena High School (California)")
-target_page = wiki.page("Rose Parade")
+target_page = wiki.page("World War II")
 wikipedia_game_solver(start_page, target_page)
-
-print(fetch_links(start_page))
+print(path)
